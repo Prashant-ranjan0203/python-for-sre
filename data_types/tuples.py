5cc791a5a0dd63ade9bc3b2fb8f1822183453000
Prashant Singh <alokranjan2034@gmail.com>
2026-03-26 05:35:47 +0000

    Added tuple immutability example

--- data_types/tuples.py
+++ data_types/tuples.py
@@ -15,3 +15,11 @@ print(f"First 2 elements: {my_tuple[:2]}")  # includes index 0,1
 print(f"From index 1 onwards: {my_tuple[1:]}") 
 #check len
 print(f"Length: {len(my_tuple)}")
+
+#immutability test 
+
+try:
+    my_tuple[0] = 10
+except TypeError as e:
+    print(f"Error:{e}")
+
