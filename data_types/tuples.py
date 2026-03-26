13ef18e39686c126e4cfc092395e0696d106f6b4
Prashant Singh <alokranjan2034@gmail.com>
2026-03-26 05:30:41 +0000

    Added tuple basics: indexing, slicing, length examples

--- data_types/tuples.py
+++ data_types/tuples.py
@@ -1,5 +1,17 @@
 # tuple example
 
-my_tuple = (1, 2, 3)
+my_tuple = (5, 6, 7)
 
-print(my_tuple)
+print(f"Tuple:{my_tuple}")
+
+#access element 
+print(f"first element :{my_tuple[0]}" )
+
+print(f"last element :{my_tuple[-1]}")
+
+##Slicing
+print(f"All elements: {my_tuple[:]}") # includes index 0,1,2
+print(f"First 2 elements: {my_tuple[:2]}")  # includes index 0,1
+print(f"From index 1 onwards: {my_tuple[1:]}") 
+#check len
+print(f"Length: {len(my_tuple)}")
