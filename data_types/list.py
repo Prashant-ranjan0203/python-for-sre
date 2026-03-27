1cedbd7b6e157e2dbdcd36ccf28dd9bf480136e1
Prashant Singh <alokranjan2034@gmail.com>
2026-03-27 02:25:03 +0000

    Added update element && Append elemnt

--- data_types/list.py
+++ data_types/list.py
@@ -8,6 +8,16 @@ print(f"List: {my_list}")
 
 print(f"First element: {my_list[0]}")
 print(f"Last element: {my_list[-1]}")
-print(f"Every 2nd element: {my_list[1::2]}")
+print(f"Every 2nd element from index 1: {my_list[1::2]}")
 ## [start : end : step] slicing works 
+
+## Update elements 
+
+my_list[0] = 100 #Lists are mutable → you can change values
+print(f"New list : {my_list}")
+
+## Appened elements
+my_list.append(50)
+print(f"After append : {my_list}")
+
 
