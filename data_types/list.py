57918a5441dc4c7e5372a8c4a7438ef1867b01e4
Prashant Singh <alokranjan2034@gmail.com>
2026-03-27 02:12:45 +0000

    Added slicing , Access elements in list

--- data_types/list.py
+++ data_types/list.py
@@ -2,3 +2,12 @@
 
 my_list = [10,20,30,40]
 print(f"List: {my_list}")
+
+
+## Access elements 
+
+print(f"First element: {my_list[0]}")
+print(f"Last element: {my_list[-1]}")
+print(f"Every 2nd element: {my_list[1::2]}")
+## [start : end : step] slicing works 
+
