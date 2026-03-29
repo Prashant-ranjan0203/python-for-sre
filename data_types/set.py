5007f07c303c9fbe6f6f742f7887b5771d034359
Prashant Singh <alokranjan2034@gmail.com>
2026-03-29 15:58:17 +0000

    set examples

---
+++ data_types/set.py
@@ -0,0 +1,6 @@
+# Simple Set Example for SRE Use Case
+
+# List with duplicate servers (common in logs or files)
+servers = ["web01", "web02", "web01", "web03", "web02"]
+print("original list :")
+print(servers)
