e7f22c3c2249e9b4b55302a23d725c80c711fc5b
Prashant Singh <alokranjan2034@gmail.com>
2026-03-29 16:02:47 +0000

    remove duplicates with set

--- data_types/set.py
+++ data_types/set.py
@@ -4,3 +4,8 @@
 servers = ["web01", "web02", "web01", "web03", "web02"]
 print("original list :")
 print(servers)
+
+# Convert list to set (removes duplicates)
+unique_servers = set(servers)
+print("\n Unique_servers (duplicates removed): ")
+print(unique_servers)
