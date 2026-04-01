c05c185cd0f6ce79c484ff278cd79ca9e6eec2c3
Prashant Singh <alokranjan2034@gmail.com>
2026-04-01 18:01:48 +0000

    create a dictionary

---
+++ data_types/Customer_Profile_Management.py
@@ -0,0 +1,12 @@
+# Create a dictionary named customer with "name": "John Doe" , "age": 32 , "city": "New York"
+customer = {
+    "name" : "John Doe",
+    "age"  :  32,
+    "city" : "New york",
+    "Email": "johndoe32@gmail.com",
+    "Phone": 9718890545
+
+
+}
+
+print (customer)
