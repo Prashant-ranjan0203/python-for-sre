3c642ab58cbd51e90569384a2002ec30be427639
Prashant Singh <alokranjan2034@gmail.com>
2026-04-03 09:54:00 +0000

    project Movie ticket pricing

---
+++ control_flow/Movie Ticket Pricing.py
@@ -0,0 +1,16 @@
+age = int(input("Enter your age: "))
+
+if age < 12 :
+    print(f" for age {age} price of ticket is ruppes 5")
+
+elif age <= 59 :
+    print(f" for age {age} price of ticket is ruppes 10")
+
+elif age >= 60 :
+    print(f" for age {age} price of ticket is ruppes 7")
+
+else :
+    print("Invalid age")
+
+
+
