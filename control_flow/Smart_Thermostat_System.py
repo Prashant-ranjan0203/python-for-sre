1edb9a79732561bdaca7a4191e925e137503ab0c
Prashant Singh <alokranjan2034@gmail.com>
2026-04-04 09:25:41 +0000

    smart thermostat project

---
+++ control_flow/Smart_Thermostat_System.py
@@ -0,0 +1,14 @@
+device_status = "active"
+temperature = 38
+
+if device_status == "active":
+    if temperature > 35:
+        print("High temperature alert !")
+
+    else:
+        ("temperature is normal")
+
+
+
+else:
+    print("Device is offline")
